70c8c625ffabe6fbd420afcb934f8aea1fd19674
WillmanGZ <willmang@uninorte.edu.co>
2025-10-05 17:28:26 -0500

    feat: recycle logic

--- src/components/MyCounterScript.vue
+++ src/components/MyCounterScript.vue
@@ -11,13 +11,12 @@
 </template>
 
 <script lang="ts">
-import { computed, ref, defineComponent } from 'vue';
-
+import { defineComponent } from 'vue';
+import { useCounter } from '@/composables/useCounter';
 export default defineComponent({
   props: { value: { type: Number, required: true } },
   setup(props) {
-    const counter = ref(props.value);
-    const squareCounter = computed(() => counter.value * counter.value);
+    const { counter, squareCounter } = useCounter(props.value);
 
     return {
       counter,
