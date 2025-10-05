be575e7cb89c0ece3e7750214b3e5b4e4bcbab2f
WillmanGZ <willmang@uninorte.edu.co>
2025-10-05 16:46:23 -0500

    feat: separate ts logic

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <h1>Mi Primera App</h1>
   <hr />
 
-  <MyCounter :value="5" />
+  <MyCounter :value="1" />
+  <MyCounterScript :value="2" />
+  <MyCounterScript2 :value="3"/>
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/MyCounter/MyCounterScript2.vue';
 </script>
---
+++ src/components/MyCounter/MyCounterScript2.ts
@@ -0,0 +1,14 @@
+import { computed, ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: { value: { type: Number, required: true } },
+  setup(props) {
+    const counter = ref(props.value);
+    const squareCounter = computed(() => counter.value * counter.value);
+
+    return {
+      counter,
+      squareCounter,
+    };
+  },
+});
