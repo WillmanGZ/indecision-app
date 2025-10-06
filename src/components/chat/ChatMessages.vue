41be4c1da3d7c3380a0bca77f9e3cb57379688bc
WillmanGZ <willmang@uninorte.edu.co>
2025-10-06 09:57:45 -0500

    feat: implement scroll after api message

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
@@ -10,10 +10,23 @@
 <script setup lang="ts">
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import ChatBubble from './ChatBubble.vue';
+import { ref, watchEffect } from 'vue';
 
 interface Props {
   messages: ChatMessage[];
 }
 
-defineProps<Props>();
+const { messages } = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null);
+
+watchEffect(() => {
+  console.log('Mensajes: ' + messages.length);
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
