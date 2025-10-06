e9f2580e0467fb14476ca6a8064b0bc7797e4335
WillmanGZ <willmang@uninorte.edu.co>
2025-10-06 09:40:23 -0500

    feat: implement yes-no api

--- src/composables/useChat.ts
+++ src/composables/useChat.ts
@@ -1,14 +1,34 @@
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import type { YesNoResponse } from '@/interfaces/yes-no.response';
 import { ref } from 'vue';
 
 export const useChat = () => {
   const messages = ref<ChatMessage[]>([]);
+  const getHerResponse = async () => {
+    const resp = await fetch('https://yesno.wtf/api');
+    const data = (await resp.json()) as YesNoResponse;
 
-  const onMessage = (text: string) => {
+    return data;
+  };
+
+  const onMessage = async (text: string) => {
+    if (text.length === 0) return;
+
     messages.value.push({
       id: new Date().getTime(),
       itsMine: true,
       message: text,
+    });
+
+    if (!text.endsWith('?')) return;
+
+    const { answer, image } = await getHerResponse();
+
+    messages.value.push({
+      id: new Date().getTime(),
+      itsMine: false,
+      message: answer,
+      image: image,
     });
   };
 
